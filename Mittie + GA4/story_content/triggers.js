84bfe24aa4340c53e4b7ebd5ae67ec5b58fde4c2
eullith <44780054+eullith@users.noreply.github.com>
2025-06-28 20:46:20 -0600

    Mittie + GA4

---
+++ Mittie + GA4/story_content/triggers.js
@@ -0,0 +1,30 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6hMsw1hV8Qx":
+        Script1();
+        break;
+      case "6OJXQLiZAOb":
+        Script2();
+        break;
+      case "6LRmcXvaEot":
+        Script3();
+        break;
+      case "5f36FiBGqZo":
+        Script4();
+        break;
+      case "5ag4eYzq08b":
+        Script5();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
