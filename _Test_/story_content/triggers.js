99f731d39a78564495025f2b2af3c2bc93e29972
eullith <44780054+eullith@users.noreply.github.com>
2025-06-30 21:20:38 -0600

    Changes

---
+++ _Test_/story_content/triggers.js
@@ -0,0 +1,42 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5VtUvxi4pIF":
+        Script1();
+        break;
+      case "6FsjH9KAwiv":
+        Script2();
+        break;
+      case "66KiJ4xZaLk":
+        Script3();
+        break;
+      case "5r4AEkkEph7":
+        Script4();
+        break;
+      case "6FmK6VJFle6":
+        Script5();
+        break;
+      case "63rSDPAOuK9":
+        Script6();
+        break;
+      case "67ih9wTyXAL":
+        Script7();
+        break;
+      case "5ug1jN1scR5":
+        Script8();
+        break;
+      case "5mDEIm1j6MO":
+        Script9();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
