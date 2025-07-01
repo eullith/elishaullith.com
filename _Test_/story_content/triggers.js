6dc085cdd48b07d8e8d11a4e295b1c0ec1c7d50d
eullith <44780054+eullith@users.noreply.github.com>
2025-06-30 20:52:31 -0600

    _test_ added

---
+++ _Test_/story_content/triggers.js
@@ -0,0 +1,42 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6EA4UOvcgcK":
+        Script1();
+        break;
+      case "6SDX9Qoj8Jz":
+        Script2();
+        break;
+      case "5fz1IWXQSeq":
+        Script3();
+        break;
+      case "6dSxAxtbWd7":
+        Script4();
+        break;
+      case "5kbVeBlZRMS":
+        Script5();
+        break;
+      case "6G0ew2GuHhQ":
+        Script6();
+        break;
+      case "640CflVbGz4":
+        Script7();
+        break;
+      case "6PlW3puJwuz":
+        Script8();
+        break;
+      case "6ZUPCMyXSfg":
+        Script9();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
